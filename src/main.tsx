caa8b504817427f3056c5ce5223cc353c4599df9
kimdowan <kimdowan1004@naver.com>
2025-08-24 15:56:12 +0900

    feat: 절대경로 alias 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@emotion/react'
-import App from './App.tsx'
-import { theme } from './styles/theme'
-import { GlobalStyles } from './styles/global'
+import App from '@/App.tsx'
+import { theme } from '@/styles/theme'
+import { GlobalStyles } from '@/styles/global'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
