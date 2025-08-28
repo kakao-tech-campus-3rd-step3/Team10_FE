73609ca9102a9a3e8e265f16f2e86d6d9c739035
MINKI SUNG <162106492+wannagola@users.noreply.github.com>
2025-08-28 20:19:17 +0900

    feat : homepage 초기 UI 제작

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from '@emotion/react'
-import App from '@/App.tsx'
-import { theme } from '@/styles/theme'
-import { GlobalStyles } from '@/styles/global'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import HomePage from '@/Pages/HomePage';
+import { theme } from '@/styles/theme';
+import { GlobalStyles } from '@/styles/global';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <App />
+      <HomePage />
     </ThemeProvider>
   </StrictMode>,
-)
+);
