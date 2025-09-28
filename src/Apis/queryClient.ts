f44bc183cf1bb618416eaf382a1b214e747a8a2f
kimdowan <kimdowan1004@naver.com>
2025-09-28 21:40:51 +0900

    fix: 4xx에러, shouldRetry 여부 로직 분리로 가독성, 유지보수성 향상

--- src/Apis/queryClient.ts
+++ src/Apis/queryClient.ts
@@ -1,6 +1,21 @@
 import { QueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
+const isClientError = (error: unknown): boolean => {
+  return (
+    error instanceof AxiosError &&
+    error.response?.status != null &&
+    error.response.status >= 400 &&
+    error.response.status < 500
+  );
+};
+const shouldRetry = (failureCount: number, error: unknown): boolean => {
+  if (isClientError(error)) {
+    return false;
+  }
+  return failureCount < 1;
+};
+
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -9,17 +24,7 @@ export const queryClient = new QueryClient({
       gcTime: 10 * 60 * 1000,
     },
     mutations: {
-      retry: (failureCount, error) => {
-        if (
-          error instanceof AxiosError &&
-          error.response?.status &&
-          error.response.status >= 400 &&
-          error.response.status < 500
-        ) {
-          return false;
-        }
-        return failureCount < 1;
-      },
+      retry: shouldRetry,
     },
   },
 });
